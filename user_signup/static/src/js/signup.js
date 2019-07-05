c6f9c14b3e91ec11a70a3b22d964a47856086bda
@r.pikh <r.pikh@seclgroup.com>
2019-07-05 15:18:44 +0300

    user

--- user_signup/static/src/js/signup.js
+++ user_signup/static/src/js/signup.js
@@ -13,14 +13,12 @@ base.ready().then(function() {
             $btn.prepend('<i class="fa fa-refresh fa-spin"/> ');
         });
     }
-});
-
 
 $('.btnNext').click(function(){
   $('.nav-tabs > .active').next('li').find('a').trigger('click');
 });
+});
 
-  $('.btnPrevious').click(function(){
-  $('.nav-tabs > .active').prev('li').find('a').trigger('click');
 });
-});
+
+
